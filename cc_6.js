460f2fcfc4c432e6ada5e83eeca9c1fd7b8c5a96
avery.white <averydouglascwhite@usf.edu>
2025-02-07 15:42:21 -0500

    Task 1 - Business Profit Calculation

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,9 @@
 console.log(`CC_6.js has succesfully been uploaded!`)
+// Task 1 - Function Declaration
+function calculateProfit(costPrice, sellingPrice, UnitsSold) {
+    let profit = (sellingPrice - costPrice) * UnitsSold;
+    return `Total Profit: $${profit}`;
+}
+console.log(calculateProfit(20, 30, 100));
+console.log(calculateProfit(50, 70, 200));
+// Task 1 - End
