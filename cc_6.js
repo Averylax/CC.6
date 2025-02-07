9d59f55ef148f5a40eef00acce8c4cbcd7d78c56
avery.white <averydouglascwhite@usf.edu>
2025-02-07 15:55:46 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -1,5 +1,5 @@
 console.log(`CC_6.js has succesfully been uploaded!`)
-// Task 1 - Function Declaration
+// Task 1 - Business Profit Calculations
 function calculateProfit(costPrice, sellingPrice, UnitsSold) {
     let profit = (sellingPrice - costPrice) * UnitsSold;
     return `Total Profit: $${profit}`;
@@ -7,3 +7,36 @@ function calculateProfit(costPrice, sellingPrice, UnitsSold) {
 console.log(calculateProfit(20, 30, 100));
 console.log(calculateProfit(50, 70, 200));
 // Task 1 - End
+
+// Task 2 - Sales Tax Computation
+const calculateSalesTax = function(amount, taxRate) {
+    let salesTax = amount * taxRate;
+    return `Sales Tax is: $${salesTax}`;
+    };
+console.log(calculateSalesTax(100, 0.07));
+console.log(calculateSalesTax(500, 0.1));
+// Task 2 - End
+
+// Task 3 - Employee Bonus Calculation
+
+// Task 3 - End
+
+// Task 4 - Subscription Pricing Module
+
+// Task 4 - End
+
+// Task 5 - Currency Conversion
+
+// Task 5 - End
+
+// Task 6 - Higher-Order Function for Bulk Orders
+
+// Task 6 - End
+
+// Task 7 - Business Expense Tracker
+
+// Task 7 - End
+
+// Task 8 - Employee Promotion Evaluation
+
+// Task 8 - End
